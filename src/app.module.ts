c2b6f33e364d3b4eba857e342cfc2b4979a3c15a
usman kiyani <usman.kiyani@ceative.co.uk>
2023-09-05 21:51:44 +0500

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
 @Module({
-  imports: [UserModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
